81bb04bd6985ed1158dd54f142be644bcaad960e
Everton Ferreira <everton.ferreira@hotmail.com>
2019-05-16 22:13:05 -0300

    snake control

--- src/snake.py
+++ src/snake.py
@@ -2,10 +2,10 @@ import pygame
 import random
 from pygame.locals import *
 
-UP = 8
-DOWN = 2
-RIGHT = 6
-LEFT = 4
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
@@ -16,15 +16,27 @@ snake_skin = pygame.Surface((10, 10))
 snake_skin.fill((255, 255, 125))  # rgb
 my_direction = LEFT
 
+clock = pygame.time.Clock()
+
 apple_pos = (random.randint(0, 590), random.randint(0, 590))
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))  # rgb
 
 while True:
-
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+            
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_LEFT:
+                my_direction = LEFT
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
 
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
@@ -33,8 +45,11 @@ while True:
     if my_direction == RIGHT:
         snake[0] = (snake[0][0] + 10, snake[0][1])
     if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1] - 10)
+        snake[0] = (snake[0][0] - 10, snake[0][1])
 
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] =  (snake[i-1][0], snake[i-1][1])
+        
     screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
     for pos in snake:
