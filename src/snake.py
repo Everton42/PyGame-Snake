9733c49adaf2a95dc79ee6f990d4f444c791c57c
Everton Ferreira <everton.ferreira@hotmail.com>
2019-05-16 22:22:30 -0300

    grid and collision

--- src/snake.py
+++ src/snake.py
@@ -2,6 +2,16 @@ import pygame
 import random
 from pygame.locals import *
 
+
+def on_grid_random():
+    x = random.randint(0, 590)
+    y = random.randint(0, 590)
+    return (x//10 * 10, y//10 * 10)
+
+def collision(c1 , c2):
+    return (c1[0] == c2[0] and (c1[1] == c2[1]))
+
+
 UP = 0
 DOWN = 1
 RIGHT = 2
@@ -18,7 +28,7 @@ my_direction = LEFT
 
 clock = pygame.time.Clock()
 
-apple_pos = (random.randint(0, 590), random.randint(0, 590))
+apple_pos = on_grid_random()
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))  # rgb
 
@@ -27,7 +37,7 @@ while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            
+
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 my_direction = UP
@@ -37,6 +47,9 @@ while True:
                 my_direction = LEFT
             if event.key == K_RIGHT:
                 my_direction = RIGHT
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
 
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
@@ -48,8 +61,8 @@ while True:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
     for i in range(len(snake) - 1, 0, -1):
-        snake[i] =  (snake[i-1][0], snake[i-1][1])
-        
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
     screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
     for pos in snake:
